c75c6afd4a664b30d28124bd2ab8788f83465d09
Ivliro <eddiesnv@gmail.com>
2025-01-02 10:58:53 +0100

    prints sorted count of letters (lowercased) in file

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 def main():
-    with open("books/frankenstein.txt") as f:
+    path = "books/frankenstein.txt"
+    with open(path) as f:
         file_contents = f.read()
 
-    print(count_words(file_contents))
-    print(count_characters(file_contents))
+    print(f"""--- Begin report of {path} ---
+{count_words(file_contents)} words found in the document
+          """)
+    
+    chars = count_characters(file_contents)
+    
+    for char in sorted(chars.items(), key=lambda x: x[1], reverse=True):
+        if char[0].isalpha():
+            print(f"The '{char[0]}' character was found {chars[char[0]]} times")
+    
+    print("--- End report ---")
 
 
 def count_words(text):

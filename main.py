e29f8e028ee5f21c7e7f27e6e02fd82cc01f9b73
Ivliro <eddiesnv@gmail.com>
2025-01-02 10:42:18 +0100

    add couting functionalities

--- main.py
+++ main.py
@@ -2,6 +2,21 @@ def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
 
-    print(file_contents)
-    
+    print(count_words(file_contents))
+    print(count_characters(file_contents))
+
+
+def count_words(text):
+    return len(text.split())
+
+def count_characters(text):
+    dictionary = {}
+    for char in text:
+        char = char.lower()
+        if char in dictionary:
+            dictionary[char] += 1
+        else:
+            dictionary[char] = 1
+    return dictionary
+
 main()
